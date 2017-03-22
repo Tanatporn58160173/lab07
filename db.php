ff266f63c784501b8b912c287af32e8bc5614a16
Tanatporn58160173 <58160173@go.buuac.th>
2017-03-22 21:18:43 +0700

    hello world

---
+++ db.php
@@ -0,0 +1,9 @@
+
+<?php
+	$host = 'localhost';
+    $username = "it58160173";
+    $password = "cream0905";
+    $dbname = 'it58160173';
+    $conn = new mysqli($host,$username,$password,$dbname);
+    $conn->query('SET NAMES UTF8'); 
+?>
